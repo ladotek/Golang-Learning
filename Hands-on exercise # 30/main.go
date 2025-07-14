8693470d874069b3b4c6a51c2346ffc76d8edc86
ladotek <114905697+ladotek@users.noreply.github.com>
2025-07-14 20:22:33 +0200

    Hands-on exercise #30 DONE

--- Hands-on exercise # 30/main.go
+++ Hands-on exercise # 30/main.go
@@ -1,5 +1,41 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math/rand"
+)
 
+func main() {
+	for i := 0; i < 42; i++ {
+		x := rand.Intn(5)
+		fmt.Printf("Iteration %v\t", i)
+
+		switch {
+		case x == 0:
+			fmt.Printf("The value of x is %v\n", x)
+		case x == 1:
+			fmt.Printf("The value of x is %v\n", x)
+		case x == 2:
+			fmt.Printf("The value of x is %v\n", x)
+		case x == 3:
+			fmt.Printf("The value of x is %v\n", x)
+		case x == 4:
+			fmt.Printf("The value of x is %v\n", x)
+		}
+		//Another option, cleaner!
+		/*switch x {
+		case 0:
+			fmt.Printf("Iteration %v \t The value of x is %v\n", i, x)
+		case 1:
+			fmt.Printf("Iteration %v \t The value of x is %v\n", i, x)
+		case 2:
+			fmt.Printf("Iteration %v \t The value of x is %v\n", i, x)
+		case 3:
+			fmt.Printf("Iteration %v \t The value of x is %v\n", i, x)
+		case 4:
+			fmt.Printf("Iteration %v \t The value of x is %v\n", i, x)
+		}
+		*/
+
+	}
 }
